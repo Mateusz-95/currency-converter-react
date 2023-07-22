ce5f68699bd78e09772269d794ba4cb38ff279ac
Mateusz Grygorowicz <grygorowiczmusic@gmail.com>
2023-07-22 11:01:26 +0200

    mediaQuery added do ContentContainer

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -3,4 +3,9 @@ import styled from "styled-components";
 export const ContentContainer = styled.div`
   max-width: 70%;
   margin: auto;
+
+  @media (max-width: 767px) {
+    max-width: 100%;
+    margin: auto;
+  }
 `;
